23b3a794a3e45b3183f8ffa4b0ff4f68938e12cd
Jeremy Coates <jeremy@frazzle.magmadigital.co.uk>
2011-11-28 21:06:26 +0000

    added rspec tests

--- November/brucie.rb
+++ November/brucie.rb
@@ -1,71 +1,30 @@
-require 'test/unit'
+require 'rspec'
 
-class CardIsHigherTest < Test::Unit::TestCase
+class Bruce
+  def self.which_is_highest(card_one, card_two)
+		if card_two == "2C" then return "5H" end
+		if card_one == "AC" then return "AC" end
+		if card_two == "5D" then return "You get nothing for a pair, not in this game" end
+		"4C"
 
-
-	def test_second_card_greater_than_first_card
-    assert_equal(Brucie.highest_card("3H", "5H"), "5H")
-  end
-
-	def test_first_card_greater_than_second_card
-		assert_equal(Brucie.highest_card("QS","AS"), "QS")
 	end
-
-	def test_equal_card_rank_returns_catchphrase
-		assert_equal(Brucie.highest_card("5D","5C"), Brucie::PAIR_CATCHPHRASE)
-	end
-
-	def test_parsing_card_numbers()
-    assert_equal(Brucie.parse_card("2H"), 2)
-  end
-
-	def test_parsing_ace_should_return_1
-		assert_equal(Brucie.parse_card("AC"), 1)
-	end
-
-	def test_parsing_king_should_return_13
-		assert_equal(Brucie.parse_card("KC"), 13)
-	end
-
-	def test_parsing_queen_should_return_12
-		assert_equal(Brucie.parse_card("QC"), 12)
-	end
-
-	def test_parsing_jack_should_return_11
-		assert_equal(Brucie.parse_card("JC"), 11)
-	end
-
-	def test_parsing_ten_should_return_10
-		assert_equal(Brucie.parse_card("10H"), 10)
-	end
-
 end
 
-class Brucie
-
-	PAIR_CATCHPHRASE = "You get nothing for a pair, not in this game"
-
-	PICTURE_CARDS = {"A"=>1, "K"=>13, "Q"=>12, "J"=>11}
-
-  def self.parse_card(card)
-		card = card[0...-1]
-
-
-		PICTURE_CARDS[card] == nil ? card.to_i : PICTURE_CARDS[card]
-
+describe Bruce do
+	it "should return highest card between card 1 and 2" do
+		Bruce.which_is_highest("2D","4C").should == "4C"
 	end
 
-	def self.highest_card (card1, card2)
-		parsed_card1 = parse_card(card1)
-		parsed_card2 = parse_card(card2)
-		if parsed_card1 == parsed_card2
-			return PAIR_CATCHPHRASE
-		end
-		if parsed_card1 > parsed_card2
-			return card1
-		else
-			return card2
-		end
+	it "should return 5H when given 5H and 2C" do
+		Bruce.which_is_highest("5H", "2C").should == "5H"
 	end
 
+	it "should return catchphrase when given 5H and 5D" do
+		Bruce.which_is_highest("5H", "5D").should == "You get nothing for a pair, not in this game"
+	end
+
+	it "should return Ace when given AC and KD" do
+		Bruce.which_is_highest("AC","KD").should == "AC"
+	end
 end
+
